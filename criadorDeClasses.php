618812d75fd82787154e3c8943ae7cacbe9974a9
Melissa Silveira <melissas.ifpr2024@gmail.com>
2026-05-19 01:17:03 -0300

    Add files via upload

---
+++ criadorDeClasses.php
@@ -0,0 +1,14 @@
+<?php
+include "banco.php";
+
+function buscarTabelas($entidades){
+  return $entidades["nome"];
+}
+function buscarAtributos($entidade)
+{
+    global $atributos;
+  return $atributos[$entidade];
+}
+
+
+?>
