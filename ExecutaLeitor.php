b2cea77ad611403e57b54fef45a6007a83760718
Melissa Silveira <melissas.ifpr2024@gmail.com>
2026-05-19 01:52:56 -0300

    Update ExecutaLeitor.php

--- ExecutaLeitor.php
+++ ExecutaLeitor.php
@@ -2,14 +2,40 @@
 
 require_once "LeitorSQL.php";
 
+if(!is_dir("model")){
+    mkdir("model",0777,true);
+}
+
 $leitor = new LeitorSQL("framework.sql");
 
-echo "<H2>Tabelas:</h2>";
+$tabelas = $leitor->getTabelas();
+foreach ($tabelas as $tabela) {
 
-print_r($leitor->getTabelas());
+    $listaAtributos = $leitor->getAtributos($tabela);
 
-echo "<hr>";
+    $attr = "";
+    $metodos="";
 
-echo "<h2>Atributos da tabela aluno:</h2>";
+   foreach ($listaAtributos as $atributo => $chave) {
+        $attr .= "   private $".$atributo.";\n";
+        $metodos .= "function get".ucfirst($atributo)."(){\n";
+        $metodos .= "return \$this->".$atributo.";\n }\n";
+        $metodos .= "function set".ucfirst($atributo)."(\$arg){\n";
+        $metodos .= " \$this->".$atributo."=\$arg;\n }\n";
+    }
 
-print_r($leitor->getAtributos("aluno"));
+$nomeClasse=ucfirst($tabela);
+$conteudo=<<<CLASS
+<?php
+class $nomeClasse {
+  
+$attr
+  
+$metodos
+  
+}
+CLASS;
+
+    file_put_contents("model/".$nomeClasse.".php",$conteudo);
+}
+?>
