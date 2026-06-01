4b3cc47a92cd6793a9e8b4e318157962607cff67
Melissa Silveira <melissas.ifpr2024@gmail.com>
2026-06-01 20:34:15 -0300

    Update formUpload.php

--- formUpload.php
+++ formUpload.php
@@ -1,1 +1,13 @@
-
+<!doctype html>
+<html lang="en">
+<head>
+  <title>Framework MVC</title>
+</head>
+<body>
+  <form action="lerArquivo.php" method="post" enctype="multipart/form-data">
+    <label for="arquivo">Envie seu arquivo SQL</label>
+    <input type="file" name="arquivo" required><br>
+    <button type="submit">Enviar</button>
+  </form>
+</body>
+</html>
