fc8d25f1259d57e52b37048f085b9850efaf6c86
Melissa Silveira <melissas.ifpr2024@gmail.com>
2026-06-01 21:04:08 -0300

    Update ExecutaLeitor.php

--- ExecutaLeitor.php
+++ ExecutaLeitor.php
@@ -2,6 +2,17 @@
 
 require_once "LeitorSQL.php";
 
+$arquivo = $_FILES['arquivo'];
+$arquivo_tmp = $arquivo['tmp_name'];
+$arquivo_size = $arquivo['size'];
+$arquivo_name = explode('.', $arquivo['name']);
+$extensao = strtolower(end($arquivo_name));
+echo "<br>";
+if($extensao != "sql") {
+  header("location: formUpload.php?erro=0");
+} 
+move_uploaded_file($arquivo_tmp, $arquivo['name']);
+
 if(!is_dir("model")){
     mkdir("model",0777,true);
 }
